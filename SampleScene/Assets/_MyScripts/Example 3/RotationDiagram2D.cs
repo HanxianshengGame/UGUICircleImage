cad8e4af674c845579b36604a942d968c23ba883
韩镇江 <Han1534930844@163.com>
2019-09-10 15:58:04 +0800

    Have a  little Update
    Add CDCooling  base on CircleImage  and Accuracy of clicking regular and irregular polygon buttons

---
+++ SampleScene/Assets/_MyScripts/Example 3/RotationDiagram2D.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Example3
+{
+    public class RotationDiagram2D : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public Vector2 ItemSize;
+        public Sprite[] ItemSprites;
+        private List<RotationDiagramItem> itemsList;
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        /// <summary>
+        /// 创建模板Template物体
+        /// </summary>
+        /// <returns></returns>
+        private GameObject CreateTemplate()
+        {
+            GameObject item = new GameObject("Template");
+            item.AddComponent<RectTransform>().sizeDelta = ItemSize;
+            item.AddComponent<RotationDiagramItem>();
+            item.AddComponent<Image>();
+            return item;
+        }
+        private void CreateItem()
+        {
+            GameObject template = CreateTemplate();
+            RotationDiagramItem itemTemp = null;
+            //resources->prefab->->GameObject->init
+            foreach (Sprite sprite in ItemSprites)
+            {
+                itemTemp=Instantiate(template).GetComponent<RotationDiagramItem>();
+                itemTemp.SetParent(transform);
+                itemTemp.SetSprite(sprite);
+                itemsList.Add(itemTemp);
+            }
+        }
+
+    }
+}
---
+++ SampleScene/Assets/_MyScripts/Example 3/RotationDiagramItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Example3 {
+    public class RotationDiagramItem : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private Image myImage;
+        private Image MyImage
+        {
+            get {
+                if (myImage == null)
+                    myImage = GetComponent<Image>();
+                return myImage;      
+               }
+        }
+        public void SetParent(Transform parent)
+        {
+            transform.SetParent(parent);
+        }
+        public void SetSprite(Sprite sprite)
+        {
+            MyImage.sprite = sprite;
+        }
+    }
+}
